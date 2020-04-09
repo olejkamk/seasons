c60eaba7c02885053c0208a1d433d14ff690b8ac
ninjacat <omelnic@gmail.com>
2020-04-09 19:29:28 -0400

    Initial commit

--- README.md
+++
@@ -1,2 +0,0 @@
-# seasons
-Created with CodeSandbox
---
+++ src/SeasonDisplay.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const SeasonDispaly = (props) => {
+
+    console.log(props.lat);
+
+    return <div>Season Display</div>;
+};
+
+
+export default SeasonDispaly;
